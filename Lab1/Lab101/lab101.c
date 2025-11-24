dc30efbf0204008a801229fe2e1f9f3b0f08425f
Alld4y <mhek33529@gmail.com>
2025-11-24 11:43:20 +0700

    Lab1.1 fix

--- Lab1/Lab101/lab101.c
+++ Lab1/Lab101/lab101.c
@@ -3,7 +3,7 @@
 // Lab101: Print "Hello Loop!" N times or "Error" if N is non-positive or 0
 int main () {
     int N,i;
-    if( scanf("%d", &N) > 0 && N > 0 ) {
+    if ( scanf("%d", &N) > 0 && N > 0 ) {
         for ( i = 0 ; i < N ; i++ ) {
             printf("Hello Loop!\n");
         } // end for
---
+++ Lab1/Lab102/Lab102.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+
+    if (scanf("%d", &score) != 1) {
+        return 1;
+    }
+    
+    return 0;
+}
