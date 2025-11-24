71a45135607dfd52508b013e3515776c0970874c
Alld4y <mhek33529@gmail.com>
2025-11-24 12:12:03 +0700

    Lab1.2 first commit

--- Lab1/Lab102/Lab102.c
+++ Lab1/Lab102/Lab102.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int score;
+    int score; 
 
-    if (scanf("%d", &score) != 1) {
+    if (scanf("%d", &score) != 1) { // Input error
         return 1;
-    }
-    
+    } else if ( score >= 80 ) { // Grade A
+        printf("Grade A\n");
+    } else if ( score >=70 ) { // Grade B
+        printf("Grade B\n");
+    } else if ( score >= 60 ) { // Grade C
+        printf("Grade C\n");
+    } else if ( score >= 50 ) { // Grade D
+        printf("Grade D\n");
+    } else if ( score < 50 ){ // Grade F
+        printf("Grade F\n");
+    } // end if-else chain
     return 0;
 }
