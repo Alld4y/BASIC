8438e2502017f255e613c060b3b625f96bc2305e
Alld4y <mhek33529@gmail.com>
2025-11-24 14:56:38 +0700

    Lab1.3 first commit

---
+++ Lab1/Lab103/Lab103.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main () {
+    int num1, num2, operationCode, result;
+
+    if (scanf("%d %d %d", &num1, &num2, &operationCode) != 3) {
+        return 1; // Input Error
+    } else {
+        switch ( operationCode ) {
+            case 1 : // Addition
+                result = num1 + num2;
+                break;
+            case 2 : // Subtraction
+                result = num1 - num2;
+                break;
+            case 3 : // Multiplication
+                result = num1 * num2;
+                break;
+            case 4 : // Division
+                if ( num2 != 0 ) {
+                    result = num1 / num2;
+                } else {
+                    return 1;
+                } // end if-else
+                break;
+            default :
+                printf("Invalid Operation\n");
+        } // end switch
+        printf("%d\n", result);
+    } // end if-else
+    return 0;
+}
