626521c7f8486b6a93ea99753979019a35ef19b0
Alld4y <mhek33529@gmail.com>
2025-11-25 20:21:09 +0700

    Lab 1.7 first commit

---
+++ Lab1/Lab107/Lab107.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main () {
+    int customerType;
+    float consumption_kWh;
+    float totalBill = 0.0;
+
+    if ( scanf("%d %f", &customerType, &consumption_kWh) != 1 ) {
+        return 1;
+    } else if ( customerType == 1 ) {
+        if ( consumption_kWh >= 0 && consumption_kWh <=100 ) {
+            totalBill = consumption_kWh * 3;
+        } else if ( consumption_kWh > 100 ) {
+            totalBill = consumption_kWh * 4;
+        } 
+    } else if ( customerType == 2 ) {
+        if ( consumption_kWh >= 0 && consumption_kWh <= 500 ) {
+            totalBill = consumption_kWh * 5;
+        } else if ( consumption_kWh > 500 ) {
+            totalBill = consumption_kWh * 6.5;
+        }
+    } else {
+        printf("Invalid  Customer Type");
+    }
+
+    if ( totalBill > 0.0 || customerType == 3 ) {
+        printf("%.2f\n", totalBill);
+    }
+
+    return 0;
+}
