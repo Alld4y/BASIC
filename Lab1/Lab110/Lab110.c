e3668d4d7afe113b15035dc3f21f0e4aca5b6605
Alld4y <mhek33529@gmail.com>
2025-11-26 00:43:12 +0700

    Lab1.10 first commit

---
+++ Lab1/Lab110/Lab110.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+    int clearanceLevel, age, isActive;
+
+    if ( scanf("%d %d %d", &clearanceLevel, &age, &isActive) != 3 ) {
+        return 1;
+    } else if ( clearanceLevel == 3 && isActive == 1 ) {
+        printf("Access Granted");
+    } else if ( clearanceLevel == 2 && age >= 25 && isActive == 1 ) {
+        printf("Access Granted");
+    } else {
+        printf("Access Denied");
+    }
+
+    return 0;
+}
+
