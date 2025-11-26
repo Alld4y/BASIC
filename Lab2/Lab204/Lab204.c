35f26985c05841c8ca77ac4edf8767301e893073
Alld4y <mhek33529@gmail.com>
2025-11-26 12:32:59 +0700

    Lab2.4 first commit

---
+++ Lab2/Lab204/Lab204.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    float principal, monthlyPayment, interest;
+    int monthCount = 0;
+    const float INTEREST_RATE = 0.01;
+    const float PENALTY = 10.0;
+
+
+    if (scanf("%f %f", &principal, &monthlyPayment) != 2)
+    {
+        return 1;
+    }
+
+    while ( principal > 0 ) {
+        if ( monthCount >= 10 ) {
+            printf("Loan settled in 10+ months.");
+            break;
+        }
+        interest = principal * INTEREST_RATE;
+        principal = principal + interest;
+        principal = principal - monthlyPayment;
+
+        if ( monthlyPayment < interest ) {
+            principal = principal + PENALTY;
+        }
+        printf("Month %d: Remaining %.2f\n", monthCount + 1, principal);
+        monthCount = monthCount +1;
+    }
+
+    if ( principal < 10)
+    printf("Loan settled in %d months.\n", monthCount);
+
+    return 0;
+}
