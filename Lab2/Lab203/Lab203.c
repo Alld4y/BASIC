62d32d23289c407caf318ed2a2621cfbc9926320
Alld4y <mhek33529@gmail.com>
2025-11-26 17:17:35 +0700

    Lab2.3 fixed

--- Lab2/Lab203/Lab203.c
+++ Lab2/Lab203/Lab203.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 
-int main () {
+int main() {
     int termCode;
     float investmentAmount;
-    float aprRate = 0.0;
-    float totalInterest = 0.0;
+    float aprRate = 0.0f;
+    float totalInterest = 0.0f;
 
-    if ( scanf("%d %f", &termCode, &investmentAmount) != 2 ) {
+    if (scanf("%d %f", &termCode, &investmentAmount) != 2) {
         return 1;
     }
 
-    switch ( termCode ) {
-        case 1 :
-            if ( investmentAmount < 5000 ) {
-                aprRate = 0.03;
+    switch (termCode) {
+        case 1:
+            if (investmentAmount < 5000) {
+                aprRate = 0.03f;
             } else {
-                aprRate = 0.04;
+                aprRate = 0.04f;
             }
             break;
-        case 2 :
-            if ( investmentAmount < 10000 ) {
-                aprRate = 0.05;
+
+        case 2:
+            if (investmentAmount < 10000) {
+                aprRate = 0.05f;
             } else {
-                aprRate = 0.065;
+                aprRate = 0.065f;
             }
             break;
-        case 3 :
-            aprRate = 0.08;
+
+        case 3:
+            aprRate = 0.08f;
             break;
     }
 
-    if ( termCode >= 1 && termCode <= 3 ) {
+    if (termCode >= 1 && termCode <= 3) {
         totalInterest = investmentAmount * aprRate;
         printf("Total Interest Earned: %.2f\n", totalInterest);
     } else {
