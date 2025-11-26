cf838defdd3c8277a8fd7f1467d4fbf2229a645a
Alld4y <mhek33529@gmail.com>
2025-11-26 17:26:12 +0700

    lab2.3 fixed

--- Lab2/Lab203/Lab203.c
+++ Lab2/Lab203/Lab203.c
@@ -3,36 +3,34 @@
 int main() {
     int termCode;
     float investmentAmount;
-    float aprRate = 0.0f;
-    float totalInterest = 0.0f;
+    float aprRate = 0.0;
+    float totalInterest = 0.0;
 
     if (scanf("%d %f", &termCode, &investmentAmount) != 2) {
         return 1;
     }
 
-    switch (termCode) {
+    switch ( termCode ) {
         case 1:
-            if (investmentAmount < 5000) {
+            if ( investmentAmount < 5000 ) {
                 aprRate = 0.03f;
             } else {
                 aprRate = 0.04f;
             }
             break;
-
         case 2:
-            if (investmentAmount < 10000) {
+            if ( investmentAmount < 10000 ) {
                 aprRate = 0.05f;
             } else {
                 aprRate = 0.065f;
             }
             break;
-
         case 3:
             aprRate = 0.08f;
             break;
     }
 
-    if (termCode >= 1 && termCode <= 3) {
+    if ( termCode >= 1 && termCode <= 3 ) {
         totalInterest = investmentAmount * aprRate;
         printf("Total Interest Earned: %.2f\n", totalInterest);
     } else {
