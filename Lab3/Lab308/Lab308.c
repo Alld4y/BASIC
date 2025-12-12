5a87cd5bfdffa511d04c82b17f410021521ffc6d
Alld4y <mhek33529@gmail.com>
2025-12-12 23:00:06 +0700

    Lab3.8 fixed

--- Lab3/Lab308/Lab308.c
+++ Lab3/Lab308/Lab308.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 struct Campaign {
-    char name[50];
-    float productPrice;
-    int salesCount;
-    float adSpend;
+    char name[50];        // Campaign name
+    float productPrice;   // Price per product
+    int salesCount;       // Number of sales
+    float adSpend;        // Advertising cost
 };
 
 int main() {
     int N, i;
 
-    if (scanf("%d", &N) != 1) {
+    if (scanf("%d", &N) != 1) {  // Read number of campaigns
         return 1;
     }
-    struct Campaign campaigns[N];
+    struct Campaign campaigns[N]; // Array to store campaign data
 
     for (i = 0; i < N; i++) {
         float commissionRate = 0.0;
@@ -21,12 +21,15 @@ int main() {
         float totalCommission;
         float netProfitLoss;
 
+        // Read campaign data
         if (scanf("%s %f %d %f", campaigns[i].name, &campaigns[i].productPrice, &campaigns[i].salesCount, &campaigns[i].adSpend) != 4) {
             return 1;
         }
 
-        totalRevenue = campaigns[i].salesCount * campaigns[i].productPrice;
+        totalRevenue = campaigns[i].salesCount * campaigns[i].productPrice;  
+        // Calculate total revenue
 
+        // Determine commission rate based on sales
         if (campaigns[i].salesCount >= 20) {
             commissionRate = 0.20;
         } else if (campaigns[i].salesCount >= 10) {
@@ -35,10 +38,11 @@ int main() {
             commissionRate = 0.10;
         }
 
-        totalCommission = totalRevenue * commissionRate;
+        totalCommission = totalRevenue * commissionRate; // Commission calculation
 
-        netProfitLoss = totalCommission - campaigns[i].adSpend;
+        netProfitLoss = totalCommission - campaigns[i].adSpend; // Net profit/loss
 
+        // Output results
         printf("--- Campaign: %s ---\n", campaigns[i].name);
         printf("Sales: %d, Ad Spend: %.2f\n", campaigns[i].salesCount, campaigns[i].adSpend);
         printf("Rate Used: %.0f%%\n", commissionRate * 100);
