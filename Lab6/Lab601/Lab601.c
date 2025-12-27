b193a16d3b69ed5c25272ad43e731983da7ba67b
Alld4y <mhek33529@gmail.com>
2025-12-27 19:55:47 +0700

    Lab6.2 first commit

--- Lab6/Lab601/Lab601.c
+++ Lab6/Lab601/Lab601.c
@@ -4,14 +4,12 @@ void increase_value(int *number_ptr);
 
 int main() {
     int data;
-
     printf("Enter initial integer value: ");
     scanf("%d", &data);
     printf("\n--- POINTER MODIFICATION REPORT ---\n");
     printf("1. Value BEFORE function call: %d\n", data);
     increase_value(&data);
     printf("2. Value AFTER function call: %d\n", data);
-
     return 0;
 }
 
